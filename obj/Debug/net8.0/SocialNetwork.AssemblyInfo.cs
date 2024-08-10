1758d036efceee45cbf49a1fb7828745d99f8d06
hieuhs3 <hieukoi5021@gmail.com>
2024-08-10 15:55:51 +0700

    x

--- obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
+++ obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b7fb6c73627fc899fb1790eab21a13bdb37841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22c902a8b3b8ed8f071e1304b24fa521213fae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
